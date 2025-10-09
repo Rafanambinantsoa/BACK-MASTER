754ba3449699ccf756c6cbb63ec36432523f1159
rafanambinantsoa <akutagawakarim@gmail.com>
2025-10-09 11:01:43 +0300

    feat: ajout de la validation des utilisateurs et mise à jour des dépendances
    - Ajout des dépendances  et  dans package.json et package-lock.json
    - Création de la classe CreateUserDto avec des validations pour les champs nom, email, statut et mot de passe
    - Mise à jour de l'entité User pour inclure le champ mot de passe
    - Implémentation de la méthode create dans UserService pour sauvegarder un nouvel utilisateur
    - Réorganisation des imports dans app.module.ts et user.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    nom: string;
+
+    @IsNotEmpty()
+    email: string;
+
+    @IsNotEmpty()
+    statut: string;
+
+    @IsNotEmpty()
+    password: string;
+
+
+}
