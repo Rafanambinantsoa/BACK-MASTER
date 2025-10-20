036767e5dda0acdda4b112a961c955265384682d
rafanambinantsoa <akutagawakarim@gmail.com>
2025-10-20 16:12:44 +0300

    feat: ajout de la configuration CORS pour gérer les origines autorisées

---
+++ src/common/config/cors.config.ts
@@ -0,0 +1,14 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:3001'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { corsConfig } from './common/config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsConfig);
 
   //Validation globale 
   // Validation globale
