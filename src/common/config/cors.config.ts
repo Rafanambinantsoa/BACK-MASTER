8a869753a616e23fc1cd4d51a5f424720d620eda
Rafanambinantsoa <akutagawakarim@gmail.com>
2026-01-14 14:49:21 +0300

    Ajout de 'http://localhost:3000' à la liste des origines autorisées dans la configuration CORS.

--- src/common/config/cors.config.ts
+++ src/common/config/cors.config.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3001'];
+    const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000'];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
