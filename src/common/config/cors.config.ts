f170029388c38b22a7c51c278eab5736e899c2e3
Rafanambinantsoa <akutagawakarim@gmail.com>
2026-02-16 11:26:28 +0300

    update cors  deploye

--- src/common/config/cors.config.ts
+++ src/common/config/cors.config.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000'];
+    const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'https://os-kim.vercel.app'];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
