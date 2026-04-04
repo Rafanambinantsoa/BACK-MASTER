2555396a18068916104a4768a70f26508c8c359a
Rafanambinantsoa <akutagawakarim@gmail.com>
2026-04-04 10:15:44 +0300

    cors

--- src/common/config/cors.config.ts
+++ src/common/config/cors.config.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'https://os-kim.vercel.app'];
+    const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'https://os-kim.vercel.app', 'https://os-kim.unityfianar.site'];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
